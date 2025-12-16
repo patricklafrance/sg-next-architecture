0995520f4b6a505b7e20323a948362c54a13c0f2
patricklafrance <pa.lafrance@gmail.com>
2025-12-16 11:29:48 -0500

    chore(ManagementPage): update header text from 'Management' to 'Management2'

--- apps/home/modules/management/src/ManagementPage.tsx
+++ apps/home/modules/management/src/ManagementPage.tsx
@@ -19,7 +19,7 @@ export function ManagementPage() {
 
     return (
         <Div>
-            <H2>Management</H2>
+            <H2>Management2</H2>
             {canShowCharacters ? (
                 <Div>
                     {characters.map(x => {
