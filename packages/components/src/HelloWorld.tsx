842881240d704ec609908aee13a1853abd8116d9
patricklafrance <pa.lafrance@gmail.com>
2025-12-16 22:26:36 -0500

    chore(HelloWorld): update greeting text for consistency

--- packages/components/src/HelloWorld.tsx
+++ packages/components/src/HelloWorld.tsx
@@ -2,6 +2,6 @@ import { Div } from "@hopper-ui/components";
 
 export function HelloWorld() {
     return (
-        <Div backgroundColor="primary">Hello World286868!</Div>
+        <Div backgroundColor="primary">Hello World1111111111!</Div>
     );
 }
