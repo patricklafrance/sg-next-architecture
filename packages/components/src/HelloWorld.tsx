f48057e0d97c89d270aea63bdb591acf098418a8
patricklafrance <pa.lafrance@gmail.com>
2025-12-16 22:46:28 -0500

    chore(chromatic.yml): comment out branch triggers for push and pull_request events fix(HelloWorld): update greeting text for consistency

--- packages/components/src/HelloWorld.tsx
+++ packages/components/src/HelloWorld.tsx
@@ -2,6 +2,6 @@ import { Div } from "@hopper-ui/components";
 
 export function HelloWorld() {
     return (
-        <Div backgroundColor="primary">Hello World1111111111!</Div>
+        <Div backgroundColor="primary">Hello World2222222222!</Div>
     );
 }
