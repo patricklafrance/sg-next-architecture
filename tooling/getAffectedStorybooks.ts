3fa1f4795c59a8ac8554337d7bf96064ea4d2264
patricklafrance <pa.lafrance@gmail.com>
2025-12-17 12:46:14 -0500

    chore(getAffectedStorybooks): remove unused variable 'toto'

--- tooling/getAffectedStorybooks.ts
+++ tooling/getAffectedStorybooks.ts
@@ -3,8 +3,6 @@ import { appendFileSync } from "node:fs";
 
 const DefaultBranch = "main";
 
-const toto = "aa";
-
 const StorybookDependencies = {
     "@apps/packages-storybook": [
         "@packages/components"
