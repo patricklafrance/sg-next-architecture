272aa52152f547f26195ebc66b22f96b02259b95
patricklafrance <pa.lafrance@gmail.com>
2025-12-19 16:51:57 -0500

    fix(HelloWorld.tsx): update greeting message to correct text

--- packages/components/src/HelloWorld.tsx
+++ packages/components/src/HelloWorld.tsx
@@ -2,6 +2,6 @@ import { Div } from "@hopper-ui/components";
 
 export function HelloWorld() {
     return (
-        <Div backgroundColor="primary">Hello World!968797898778978979</Div>
+        <Div backgroundColor="primary">Hello World!232354658</Div>
     );
 }
