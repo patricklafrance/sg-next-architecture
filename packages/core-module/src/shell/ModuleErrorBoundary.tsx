d3ae64f06e3f57fa2f2494a545c2e399f78ab4db
patricklafrance <pa.lafrance@gmail.com>
2025-12-19 23:12:43 -0500

    style(ModuleErrorBoundary): update error message to include additional exclamation marks

--- packages/core-module/src/shell/ModuleErrorBoundary.tsx
+++ packages/core-module/src/shell/ModuleErrorBoundary.tsx
@@ -2,6 +2,6 @@ import { Text } from "@hopper-ui/components";
 
 export function ModuleErrorBoundary() {
     return (
-        <Text>An error occured while rendering a page from a module!</Text>
+        <Text>An error occured while rendering a page from a module!!!!!!</Text>
     );
 }
