7ced21f33ae1cd5db8292b4c6d9add964cb6e5a7
patricklafrance <pa.lafrance@gmail.com>
2025-12-29 12:37:04 -0500

    fix(HelloWorld): update text in HelloWorld component

--- packages/components/src/HelloWorld.tsx
+++ packages/components/src/HelloWorld.tsx
@@ -2,6 +2,6 @@ import { Div } from "@hopper-ui/components";
 
 export function HelloWorld() {
     return (
-        <Div backgroundColor="primary">Hello World35345645674!</Div>
+        <Div backgroundColor="primary">Hello World4554647!</Div>
     );
 }
